4bb941598aeaf7b70ad4278ba927dcf27a4846bf
CSeungJoo <seungjoo13579@gmail.com>
2026-01-26 20:36:11 +0900

    fix: 잘못된 gitignore 설정으로 누락된 코드 복구

---
+++ CHome_BE/src/main/java/kr/cseungjoo/chome_be/user/application/port/out/UserRepositoryPort.java
@@ -0,0 +1,16 @@
+package kr.cseungjoo.chome_be.user.application.port.out;
+
+import kr.cseungjoo.chome_be.user.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepositoryPort {
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
+
+    boolean exists(String email);
+
+    User save(User user);
+}
